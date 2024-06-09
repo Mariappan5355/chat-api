1edd1be8612162566176a722655a24dc4ae73336
Mariappan <amulmariappan@gmail.com>
2024-06-09 12:20:14 +0530

    Fix cors

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import apiRoutes from './routes/apiRoutes'
 import dotenv from 'dotenv';
 import { connectDatabase } from './database';
+const cors = require('cors');
 
 
 dotenv.config();
@@ -9,6 +10,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 app.use(express.json());
+app.use(cors());
 
 connectDatabase();
 
